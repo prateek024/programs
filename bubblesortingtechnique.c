2e65cb6da8edd548806f11167a044d03afdd129e
prateek024 <119643158+prateek024@users.noreply.github.com>
2022-12-07 11:41:11 +0530

    Create bubblesortingtechnique.c

---
+++ bubblesortingtechnique.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int binarySearch(int array[], int x, int low, int high) {
+
+  while (low <= high) {
+    int mid = low + (high - low) / 2;
+
+    if (array[mid] == x)
+      return mid;
+
+    if (array[mid] < x)
+      low = mid + 1;
+
+    else
+      high = mid - 1;
+  }
+
+  return -1;
+}
+
+int main(void) 
+{ 
+int array[1000],i,n,key,result;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+scanf("%d",array[i]);
+scanf("%d",&key);
+  result = binarySearch(array, x, 0, n - 1);
+  if (result == -1)
+    printf("Not found");
+  else
+    printf("Element is found at index %d", result);
+  return 0;
+}
